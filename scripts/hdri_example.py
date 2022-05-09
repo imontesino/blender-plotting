aae5962384cada26d6bb5c1ce46f39451c6df720
Ignacio Montesino <monte.igna@gmail.com>
2022-05-09 12:57:22 +0200

    Add ecample ofr adding a hdr background image

---
+++ scripts/hdri_example.py
@@ -0,0 +1,57 @@
+""" example of adding and hdri as a texture environment
+"""
+import math
+
+import bpy
+
+from utils.renderers import cycles_gpu_render
+from utils.background import set_hdri_background
+
+# Delete the default cube
+bpy.ops.object.delete()
+
+# Add a sphere
+bpy.ops.mesh.primitive_uv_sphere_add(radius=1, location=(0,0,1))
+sphere = bpy.context.active_object
+
+# add subdivision
+bpy.ops.object.modifier_add(type='SUBSURF')
+sphere.modifiers[0].render_levels = 6
+bpy.ops.object.mode_set(mode = 'EDIT')
+bpy.ops.mesh.faces_shade_smooth()
+
+# create material
+mat = bpy.data.materials.new(name='PlaneMaterial')
+
+#assign material to plane
+sphere.data.materials.append(mat)
+mat.use_nodes=True
+
+# let's create a variable to store our list of nodes
+mat_nodes = mat.node_tree.nodes
+
+mat_nodes['Principled BSDF'].inputs['Base Color'].default_value=(0.5, 0.5, 0.5, 1.0)
+mat_nodes['Principled BSDF'].inputs['Roughness'].default_value=0.0
+mat_nodes['Principled BSDF'].inputs['Metallic'].default_value=0.95
+
+
+C = bpy.context
+
+set_hdri_background(C.scene, "resources/hdri/dikhololo_night_8k.exr")
+
+# we first create the camera object
+cam_data = bpy.data.cameras.new('camera')
+cam = bpy.data.objects.new('camera', cam_data)
+cam.location = (2, -2, 2)
+cam.data.lens_unit = 'FOV'
+cam.data.angle = math.radians(100)
+
+# track the cube with the camera
+constraint = cam.constraints.new(type='TRACK_TO')
+constraint.target=sphere
+
+# add camera to scene
+scene = bpy.context.scene
+scene.camera = cam
+
+cycles_gpu_render(scene, 'hdri_example.png')
