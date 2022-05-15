932214fbe6e74305f6c27ff4b2af6b752b7063bf
Ignacio Montesino <monte.igna@gmail.com>
2022-05-15 20:01:56 +0200

    prevent segfault when subsurface modifiers are used

--- blender_plotting/utils/renderers.py
+++ blender_plotting/utils/renderers.py
@@ -3,25 +3,30 @@ import bpy
 
 
 def cycles_render(scene: bpy.types.Scene,
-                  image_path: str,
+                  file_name: str,
                   use_gpu: bool=True,
                   resolution_x: int=1920,
                   resolution_y:int =1080,
-                  samples: int=128):
+                  samples: int=128,
+                  animation: bool=False):
     """Render a scene using th Cycles engine.
 
     Args:
         scene (bpy.types.Scene): The scene to render.
-        image_path (str): The path to save the image to.
+        file_name (str): The path to save the image to.
         use_gpu (bool, optional): use GPU accelerated Rendering. Defaults to True.
         resolution_x (int, optional): Defaults to 1920.
         resolution_y (int, optional): Defaults to 1080.
         samples (int, optional): Defaults to 128.
     """
+    if animation:
+        file_name = "animation/" + file_name
+        scene.render.filepath = file_name+".avi"
+    else:
+        scene.render.image_settings.file_format = 'PNG'
+        scene.render.filepath = file_name+".png"
 
     scene.render.engine = 'CYCLES'
-    scene.render.image_settings.file_format = 'PNG'
-    scene.render.filepath = image_path
 
     if use_gpu:
         # Set the device_type
@@ -62,17 +67,18 @@ def cycles_render(scene: bpy.types.Scene,
     # set samples
     bpy.context.scene.cycles.samples = samples
 
-    bpy.ops.render.render(write_still=1)
+    bpy.ops.render.render(write_still=1, animation=animation)
 
 def eevee_render(scene: bpy.types.Scene,
-                 image_path: str,
+                 file_name: str,
                  resolution_x: int=1920,
-                 resolution_y:int =1080):
+                 resolution_y:int =1080,
+                 animation: bool=False):
     """Render a scene using the Eevee engine.
 
     Args:
         scene (bpy.types.Scene): The scene to render.
-        image_path (str): The path to save the image to.
+        file_name (str): The path to save the image to.
         use_gpu (bool, optional): use GPU accelerated Rendering. Defaults to True.
         resolution_x (int, optional): Defaults to 1920.
         resolution_y (int, optional): Defaults to 1080.
@@ -81,25 +87,42 @@ def eevee_render(scene: bpy.types.Scene,
     # TODO find cause
     rm_objects = remove_subsurf_modifiers(scene)
 
+    if animation:
+        file_name = "animation/" + file_name
+        scene.render.filepath = file_name+".avi"
+    else:
+        scene.render.image_settings.file_format = 'PNG'
+        scene.render.filepath = file_name+".png"
+
     if rm_objects:
         print("Warning: Removing subsurf modifiers from the following objects:")
         print(rm_objects)
 
     scene.render.engine = 'BLENDER_EEVEE'
-    scene.render.image_settings.file_format = 'PNG'
-    scene.render.filepath = image_path
     scene.render.resolution_x = resolution_x
     scene.render.resolution_y = resolution_y
     scene.render.resolution_percentage = 100
-    bpy.ops.render.render(write_still=1)
+    bpy.ops.render.render(write_still=1, animation=animation)
 
 def workbench_render(scene: bpy.types.Scene,
-                     image_path: str,
+                     file_name: str,
                      resolution_x: int=1920,
-                     resolution_y: int=1080):
+                     resolution_y: int=1080,
+                     animation: bool=False):
+    """ Render a scene using the Workbench engine.
+
+    Very fast render for prototiping animations and positions.
+    """
     # Prevent segfault
     # TODO find cause
     rm_objects = remove_subsurf_modifiers(scene)
+
+    if animation:
+        file_name = "animation/" + file_name
+        scene.render.filepath = file_name+".avi"
+    else:
+        scene.render.image_settings.file_format = 'PNG'
+        scene.render.filepath = file_name+".png"
 
     if rm_objects:
         print("Warning: Removing subsurf modifiers from the following objects:")
@@ -111,16 +134,11 @@ def workbench_render(scene: bpy.types.Scene,
     shading = scene.display.shading
     shading.light = 'STUDIO'
     shading.color_type = 'TEXTURE'
-    shading.show_xray = True
 
-    bpy.context.preferences.themes[0].view_3d.space.gradients.high_gradient = (1.0, 0.0, 0.0)
-
-    scene.render.image_settings.file_format = 'PNG'
-    scene.render.filepath = image_path
     scene.render.resolution_x = resolution_x
     scene.render.resolution_y = resolution_y
     scene.render.resolution_percentage = 100
-    bpy.ops.render.render(write_still=1)
+    bpy.ops.render.render(write_still=1, animation=animation)
 
 
 def remove_subsurf_modifiers(scene: bpy.types.Scene) -> List[str]:
