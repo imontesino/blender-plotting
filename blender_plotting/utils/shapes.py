d8a9592000059588cb33c072c36b3b104b2be5f6
Ignacio Montesino <monte.igna@gmail.com>
2022-06-01 10:06:28 +0200

    Better arrow creation with parameters

--- blender_plotting/utils/shapes.py
+++ blender_plotting/utils/shapes.py
@@ -9,6 +9,7 @@ from blender_plotting.utils.materials import create_solid_material
 def add_arrow(vector_origin: np.ndarray,
               vector_end: np.ndarray,
               name: Optional[str] = None,
+              thickness: float = 1,
               color: tuple = (1, 0, 0, 1.0)) -> Tuple[bpy.types.Object, bpy.types.Object]:
     """Add a 3D arrow model to the scene.
 
@@ -16,6 +17,7 @@ def add_arrow(vector_origin: np.ndarray,
         vector_origin (np.ndarray): Origin of the arrow.
         vector_end (np.ndarray): End of the arrow.
         name (Optional[str]): Name of the arrow.
+        thickness (float): relative thickness of the arrow.
         color (tuple): Color of the arrow.
 
     Returns:
@@ -36,10 +38,12 @@ def add_arrow(vector_origin: np.ndarray,
     else:
         rot = mathutils.Quaternion((1,0,0),np.pi)
 
+    head_ratio = 0.2 + 0.3 * np.clip((thickness - 1)/3, 0, 1)
+
     # Create the cylinder part
-    cyl_length = v.length*0.8
+    cyl_length = v.length*(1-head_ratio)
     bpy.ops.mesh.primitive_cylinder_add(vertices=16,
-                                        radius=0.01,
+                                        radius=v.length*0.01*thickness,
                                         depth=cyl_length,
                                         end_fill_type='NGON',
                                         enter_editmode=False)
@@ -51,9 +55,9 @@ def add_arrow(vector_origin: np.ndarray,
         cylinder.name = name + '_body'
 
     # Create the cone part
-    cone_length = v.length*0.2
+    cone_length = v.length*head_ratio
     bpy.ops.mesh.primitive_cone_add(
-        radius1=0.05,
+        radius1=v.length*0.05*thickness,
         radius2=0,
         depth=cone_length,
         enter_editmode=False)
